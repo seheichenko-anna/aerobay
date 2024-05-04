d907ba2889aa3c5777d0e3f2975fb0806cfc4d71
seheichenko-anna <seheichenko.anna@gmail.com>
2024-05-04 17:02:39 +0300

    Add hover effects

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -3,12 +3,14 @@ import svg from '../../assets/sprite.svg';
 import s from './LanguageSelector.module.css';
 
 const LanguageSelector = () => {
-  const [isOpenDropDown, setIsOpenDropdown] = useState<boolean>(false);
+  const [isOpenDropdown, setIsOpenDropdown] = useState<boolean>(false);
+  const [lang, setLang] = useState<string>('en');
   const dropdownRef = useRef<HTMLUListElement>(null);
+  const btnRef = useRef<HTMLButtonElement>(null);
 
   const toggleDropDown = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation();
-    setIsOpenDropdown(!isOpenDropDown);
+    setIsOpenDropdown(!isOpenDropdown);
   };
 
   const handleClickOutside = (event: MouseEvent) => {
@@ -21,7 +23,8 @@ const LanguageSelector = () => {
   };
 
   useEffect(() => {
-    if (isOpenDropDown) {
+    if (isOpenDropdown && btnRef.current) {
+      btnRef.current.focus();
       window.addEventListener('click', handleClickOutside);
     } else {
       window.removeEventListener('click', handleClickOutside);
@@ -30,21 +33,26 @@ const LanguageSelector = () => {
     return () => {
       window.removeEventListener('click', handleClickOutside);
     };
-  }, [isOpenDropDown]);
+  }, [isOpenDropdown]);
+
+  const handleSelectLanguage = (lang: string): void => {
+    setLang(lang);
+    setIsOpenDropdown(false);
+  };
 
   return (
     <div className={s.language_selector}>
-      <button className={s.icon_wrapper} onClick={toggleDropDown}>
+      <button ref={btnRef} className={s.icon_wrapper} onClick={toggleDropDown}>
         <svg className={s.icon_globe}>
           <use xlinkHref={`${svg}#icon-globe`} />
         </svg>
       </button>
-      {isOpenDropDown && (
+      {isOpenDropdown && (
         <ul ref={dropdownRef} className={s.dropdown}>
-          <li>
+          <li onClick={() => handleSelectLanguage('en')}>
             <p>English</p>
           </li>
-          <li>
+          <li onClick={() => handleSelectLanguage('ua')}>
             <p>Ukrainian</p>
           </li>
         </ul>
