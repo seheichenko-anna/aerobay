f69cfae022a7c90438c5f8104218570f2d62d994
Misha_Bardyn <bardynmisha@gmail.com>
2024-05-06 16:53:31 +0300

    Adding styles to ActionCallBlock

--- src/components/ActionCallBlock/ActionCallBlock.tsx
+++ src/components/ActionCallBlock/ActionCallBlock.tsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 import s from './ActionCallBlock.module.css';
-import { MdArrowOutward } from "react-icons/md";
+import { MdArrowOutward } from 'react-icons/md';
 
 function ActionCallBlock() {
     return (
         <div className={s.container}>
-            <h1 className={s.container_title}>Already Know What <span>You Want</span>?</h1>
+            <h1 className={s.container_title}><span>Already Know</span> What <span>You Want</span>?</h1>
             <Link to="/catalog">
                 <button className={s.container_button}>Go to catalog <span><MdArrowOutward size={20} /></span></button>
             </Link>
