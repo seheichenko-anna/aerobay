df82470f27d755a04b4ee113ab5a77f8ad7c41e4
seheichenko-anna <seheichenko.anna@gmail.com>
2024-05-06 15:51:48 +0300

    Init

---
+++ src/components/ThreeTypesOfDrones/ThreeTypesOfDrones.tsx
@@ -0,0 +1,14 @@
+import s from './ThreeTypesOfDrones.module.css';
+
+const ThreeTypesOfDrones = () => {
+  return (
+    <section className={s.three_drones}>
+      <h2>
+        Your Vision, Your Drone, <br /> Your Success
+      </h2>
+      <ul></ul>
+    </section>
+  );
+};
+
+export default ThreeTypesOfDrones;
