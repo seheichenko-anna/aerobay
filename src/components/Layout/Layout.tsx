99d19ced124cb2cf24fed1839393262045936d3b
Misha_Bardyn <bardynmisha@gmail.com>
2024-05-07 21:01:19 +0300

    Return to the previous state of the footer component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,12 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 
 const Layout = () => {
   return (
     <>
       <div>Header</div>
+      {/* тимчасова лінка для перевірки*/}
+      <Link to='home-page'>Return Home page</Link>
       <Outlet />
       <Footer />
     </>
