efded83f364dee54cf5272cbc4db2ca09d36b046
Misha_Bardyn <bardynmisha@gmail.com>
2024-05-12 18:48:22 +0300

    Transferring reviews component from loyaut to homePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 
 import SplitScreen from '../components/SplitScreen/SplitScreen';
 import ActionCallBlock from "../components/ActionCallBlock/ActionCallBlock";
+import Reviews from '../components/Reviews/Reviews';
 const HomePage = () => {
   return (
     <>
       <div>HomePage</div>
       <SplitScreen />
       <ActionCallBlock/>
+      <Reviews/>
     </>
   );
 };
