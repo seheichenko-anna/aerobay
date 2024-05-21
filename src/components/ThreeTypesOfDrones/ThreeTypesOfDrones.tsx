be6d39de0cb87b7bece853b340ab6114af6608a6
seheichenko-anna <seheichenko.anna@gmail.com>
2024-05-21 15:21:33 +0300

    Fix three drones section
    Add responsive

--- src/components/ThreeTypesOfDrones/ThreeTypesOfDrones.tsx
+++ src/components/ThreeTypesOfDrones/ThreeTypesOfDrones.tsx
@@ -1,40 +1,126 @@
 import s from './ThreeTypesOfDrones.module.css';
 import svg from '../../assets/sprite.svg';
 import { Link } from 'react-router-dom';
+import { useDashboard } from '../../hooks/useDashboard';
+import { Swiper } from 'swiper/react';
+import { SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import lidDrone from '../../assets/three-drones/LiDAR-research.jpg';
+import agroDrone from '../../assets/three-drones/Agribusiness.jpg';
+import droneViewer from '../../assets/three-drones/Photo-video-production.jpg';
+import { useEffect, useState } from 'react';
+import CustomPagination from '../CustomPagination/CustomPagination';
 
 const ThreeTypesOfDrones = () => {
+  const { isLaptopOrTablet, isMobileOrBigScreen, isBigScreen } = useDashboard();
+  const [swiper, setSwiper] = useState<any>(null);
+  const [activeIndex, setActiveIndex] = useState<number>(1);
+  useEffect(() => {
+    if (swiper) {
+      swiper.on('slideChange', () => {
+        setActiveIndex(swiper.realIndex);
+      });
+    }
+  }, [swiper]);
+
+  const goToSlide = (index: number) => {
+    if (swiper) {
+      swiper.slideTo(index);
+    }
+  };
+
   return (
     <section className={s.three_drones}>
       <h2 className={s.section_title}>
         Your Vision, <span className={s.title_accent}>Your Drone,</span> <br />{' '}
         Your Success
       </h2>
-      <ul className={s.three_drones_list}>
-        <li className={s.lidar_research}>
-          <Link to="lidar-drone" className={s.arrow_wrapper}>
-            <svg className={s.arrow}>
-              <use xlinkHref={`${svg}#icon-arrow-up-right`} />
-            </svg>
-          </Link>
-          <h3 className={s.drone_title}>LiDAR research</h3>
-        </li>
-        <li className={s.agribusiness}>
-          <Link to="agriculture-drone" className={s.arrow_wrapper}>
-            <svg className={s.arrow}>
-              <use xlinkHref={`${svg}#icon-arrow-up-right`} />
-            </svg>
-          </Link>
-          <h3 className={s.drone_title}>Agribusiness</h3>
-        </li>
-        <li className={s.photo_video_production}>
-          <Link to="drone-viewer" className={s.arrow_wrapper}>
-            <svg className={s.arrow}>
-              <use xlinkHref={`${svg}#icon-arrow-up-right`} />
-            </svg>
-          </Link>
-          <h3 className={s.drone_title}>Photo-video production</h3>
-        </li>
-      </ul>
+
+      {isLaptopOrTablet && (
+        <div className={s.three_drones_swiper}>
+          <Swiper
+            onSwiper={setSwiper}
+            spaceBetween={16}
+            slidesPerView={'auto'}
+            centeredSlides={true}
+            initialSlide={1}
+            className={s.swiper}
+          >
+            <SwiperSlide className={s.slide}>
+              <div className={s.slider_wrapper}>
+                <img src={lidDrone} className={s.img_drone} />
+                <Link to="lidar-drone" className={s.arrow_wrapper}>
+                  <svg className={s.arrow}>
+                    <use xlinkHref={`${svg}#icon-arrow-up-right`} />
+                  </svg>
+                </Link>
+                <h3 className={s.drone_title}>LiDAR research</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={s.slide}>
+              <div className={s.slider_wrapper}>
+                <img src={agroDrone} className={s.img_drone} />
+                <Link to="agriculture-drone" className={s.arrow_wrapper}>
+                  <svg className={s.arrow}>
+                    <use xlinkHref={`${svg}#icon-arrow-up-right`} />
+                  </svg>
+                </Link>
+                <h3 className={s.drone_title}>Agribusiness</h3>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={s.slide}>
+              <div className={s.slider_wrapper}>
+                <img src={droneViewer} className={s.img_drone} />
+                <Link to="drone-viewer" className={s.arrow_wrapper}>
+                  <svg className={s.arrow}>
+                    <use xlinkHref={`${svg}#icon-arrow-up-right`} />
+                  </svg>
+                </Link>
+                <h3 className={s.drone_title}>Photo-video production</h3>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+          <div className={s.pagination}>
+            <CustomPagination
+              activeIndex={activeIndex}
+              goToSlide={goToSlide}
+              color="black"
+            />
+          </div>
+        </div>
+      )}
+
+      {isMobileOrBigScreen && (
+        <ul className={s.three_drones_list}>
+          <li className={s.drone_wrapper}>
+            <img src={lidDrone} className={s.img_drone} />
+            <Link to="lidar-drone" className={s.arrow_wrapper}>
+              <svg className={s.arrow}>
+                <use xlinkHref={`${svg}#icon-arrow-up-right`} />
+              </svg>
+            </Link>
+            <h3 className={s.drone_title}>LiDAR research</h3>
+          </li>
+          <li className={s.drone_wrapper}>
+            <img src={agroDrone} className={s.img_drone} />
+            <Link to="agriculture-drone" className={s.arrow_wrapper}>
+              <svg className={s.arrow}>
+                <use xlinkHref={`${svg}#icon-arrow-up-right`} />
+              </svg>
+            </Link>
+            <h3 className={s.drone_title}>Agribusiness</h3>
+          </li>
+          <li className={s.drone_wrapper}>
+            <img src={droneViewer} className={s.img_drone} />
+            <Link to="drone-viewer" className={s.arrow_wrapper}>
+              <svg className={s.arrow}>
+                <use xlinkHref={`${svg}#icon-arrow-up-right`} />
+              </svg>
+            </Link>
+            <h3 className={s.drone_title}>Photo-video production</h3>
+          </li>
+        </ul>
+      )}
     </section>
   );
 };
--- src/hooks/useDashboard.tsx
+++ src/hooks/useDashboard.tsx
@@ -4,6 +4,12 @@ export const useDashboard = () => {
   const isBigScreenOrTablet = useMediaQuery({
     query: '(min-width: 768px)',
   });
+  const isLaptopOrTablet = useMediaQuery({
+    query: '(min-width: 768px) and (max-width: 1439px)',
+  });
+  const isMobileOrBigScreen = useMediaQuery({
+    query: '(max-width: 767px) or (min-width: 1440px)',
+  });
   const isBigScreen = useMediaQuery({ query: '(min-width: 1440px)' });
   const isMobileOrTablet = useMediaQuery({ query: '(max-width: 1439px)' });
   const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' });
@@ -12,5 +18,7 @@ export const useDashboard = () => {
     isBigScreen,
     isMobileOrTablet,
     isRetina,
+    isLaptopOrTablet,
+    isMobileOrBigScreen,
   };
 };
