331102584d88f263af7dbf939e46c8b7e3b30e58
Misha_Bardyn <bardynmisha@gmail.com>
2024-06-21 00:44:17 +0300

    Fixed title and added styles to button in ActionCallBlock

--- src/components/ActionCallBlock/ActionCallBlock.tsx
+++ src/components/ActionCallBlock/ActionCallBlock.tsx
@@ -5,7 +5,7 @@ import { MdArrowOutward } from 'react-icons/md';
 function ActionCallBlock() {
     return (
         <div className={s.container}>
-            <h1 className={s.container_title}><span>Already Know</span> What <span>You Want</span>?</h1>
+            <h2 className={s.container_title}><span>Already Know</span> What <span>You Want</span>?</h2>
             <Link to="/catalog">
                 <button className={s.container_button}>Go to catalog <span><MdArrowOutward size={20} /></span></button>
             </Link>
