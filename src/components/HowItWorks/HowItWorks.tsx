6babd0ec40b922fa15a9a0e428927f9749821b62
seheichenko-anna <seheichenko.anna@gmail.com>
2024-06-25 16:03:48 +0300

    Changed steps title

--- src/components/HowItWorks/HowItWorks.tsx
+++ src/components/HowItWorks/HowItWorks.tsx
@@ -1,220 +1,56 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import s from './HowItWorks.module.css';
-import svg from '../../assets/sprite.svg';
 import { useDashboard } from '../../hooks/useDashboard';
+import Step from './Step';
 
 const HowItWorks = () => {
-  const { isBigScreen, isMobileOrTablet, isLaptopOrTablet, isMobile } =
-    useDashboard();
+  const { isBigScreen, isLaptopOrTablet, isMobile } = useDashboard();
+
+  const steps = [
+    {
+      number: 1,
+      icon: 'business',
+      title: 'Сhoose a drone “ready solution“ or a base model',
+      text: 'Drone-LiDAR / Agriculture Drone / View-Drone',
+    },
+    {
+      number: 2,
+      icon: 'camera',
+      title: 'Customize your drone or slip this step',
+      text: 'Camera / battery / scanner / workstation etc.',
+    },
+    {
+      number: 3,
+      icon: 'package',
+      title: 'Place your order …',
+      text: 'Add the product to the cart and fill in all the necessary fields to complete the order',
+    },
+    {
+      number: 4,
+      icon: 'phone-call',
+      title: 'Our manager will contact you to confirm the order',
+      text: "Our manager will contact you to clarify the order, then you will receive an invoice for payment. After payment, we'll send the drone to the address you specified on the same day.",
+    },
+  ];
 
   return (
     <section className={s.section_how_it_works}>
       <h2 className={s.section_title}>how it works</h2>
-      {isMobileOrTablet && (
-        <div className={s.swiper_wrapper}>
-          <Swiper
-            spaceBetween={32}
-            slidesPerView={'auto'}
-            slidesOffsetBefore={isMobile ? -16 : 0}
-            centeredSlides={isLaptopOrTablet ? false : true}
-          >
-            <SwiperSlide className={s.slide}>
-              <div className={s.step_wrapper}>
-                <div className={`${s.number_wrapper} ${s.number_line}`}>
-                  <div className={s.number}>
-                    <p>1</p>
-                  </div>
-                </div>
-                <div className={s.step}>
-                  <div className={s.padding_wrapper}>
-                    <div className={s.icon_step_wrapper}>
-                      <svg className={s.icon_step}>
-                        <use xlinkHref={`${svg}#icon-business`} />
-                      </svg>
-                    </div>
-                  </div>
-                  <h3 className={s.step_title}>
-                    Chose your business or type of drone acording to your
-                    business
-                  </h3>
-                  <p className={s.step_text}>
-                    Drone-LiDAR / Agriculture Drone/ View-Drone
-                  </p>
-                </div>
-              </div>
+      <div className={s.swiper_wrapper}>
+        <Swiper
+          spaceBetween={32}
+          slidesPerView={isBigScreen ? 4 : 'auto'}
+          slidesOffsetBefore={isMobile ? -16 : 0}
+          centeredSlides={isLaptopOrTablet ? false : !isBigScreen}
+        >
+          {steps.map(step => (
+            <SwiperSlide key={step.number} className={s.slide}>
+              <Step {...step} />
             </SwiperSlide>
-            <SwiperSlide className={s.slide}>
-              <div className={s.step_wrapper}>
-                <div className={s.number_wrapper}>
-                  <div className={s.number}>
-                    <p>2</p>
-                  </div>
-                </div>
-                <div className={s.step}>
-                  <div className={s.padding_wrapper}>
-                    <div className={s.icon_step_wrapper}>
-                      <svg className={s.icon_step}>
-                        <use xlinkHref={`${svg}#icon-camera`} />
-                      </svg>
-                    </div>
-                  </div>
-                  <h3 className={s.step_title}>
-                    Add accessories that you need
-                  </h3>
-                  <p className={s.step_text}>
-                    Camera/battery/scanner/ workstation etc.
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className={s.slide}>
-              <div className={s.step_wrapper}>
-                <div className={s.number_wrapper}>
-                  <div className={s.number}>
-                    <p>3</p>
-                  </div>
-                </div>
-                <div className={s.step}>
-                  <div className={s.padding_wrapper}>
-                    <div className={s.icon_step_wrapper}>
-                      <svg className={s.icon_step}>
-                        <use xlinkHref={`${svg}#icon-package`} />
-                      </svg>
-                    </div>
-                  </div>
-                  <h3 className={s.step_title}>Make your order</h3>
-                  <p className={s.step_text}>
-                    add the product to the cart and fill in all the necessary
-                    fields to complete the order
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide className={s.slide}>
-              <div className={s.step_wrapper}>
-                <div className={s.number_wrapper}>
-                  <div className={s.number}>
-                    <p>4</p>
-                  </div>
-                </div>
-                <div className={s.step}>
-                  <div className={s.padding_wrapper}>
-                    <div className={s.icon_step_wrapper}>
-                      <svg className={s.icon_step}>
-                        <use xlinkHref={`${svg}#icon-phone-call`} />
-                      </svg>
-                    </div>
-                  </div>
-                  <h3 className={s.step_title}>The manager will contact you</h3>
-                  <p className={s.step_text}>
-                    Our manager will contact you to clarify the order, then you
-                    will receive an invoice for payment. After payment, we'll
-                    send the drone to the address you specified on the same day.
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      )}
-      {isBigScreen && (
-        <ul className={s.steps_list}>
-          <li>
-            <div className={s.step_wrapper}>
-              <div className={`${s.number_wrapper} ${s.number_line}`}>
-                <div className={s.number}>
-                  <p>1</p>
-                </div>
-              </div>
-              <div className={s.step}>
-                <div className={s.padding_wrapper}>
-                  <div className={s.icon_step_wrapper}>
-                    <svg className={s.icon_step}>
-                      <use xlinkHref={`${svg}#icon-business`} />
-                    </svg>
-                  </div>
-                </div>
-                <h3 className={s.step_title}>
-                  Chose your business or type of drone acording to your business
-                </h3>
-                <p className={s.step_text}>
-                  Drone-LiDAR / Agriculture Drone/ View-Drone
-                </p>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className={s.step_wrapper}>
-              <div className={s.number_wrapper}>
-                <div className={s.number}>
-                  <p>2</p>
-                </div>
-              </div>
-              <div className={s.step}>
-                <div className={s.padding_wrapper}>
-                  <div className={s.icon_step_wrapper}>
-                    <svg className={s.icon_step}>
-                      <use xlinkHref={`${svg}#icon-camera`} />
-                    </svg>
-                  </div>
-                </div>
-                <h3 className={s.step_title}>Add accessories that you need</h3>
-                <p className={s.step_text}>
-                  Camera/battery/scanner/ workstation etc.
-                </p>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className={s.step_wrapper}>
-              <div className={s.number_wrapper}>
-                <div className={s.number}>
-                  <p>3</p>
-                </div>
-              </div>
-              <div className={s.step}>
-                <div className={s.padding_wrapper}>
-                  <div className={s.icon_step_wrapper}>
-                    <svg className={s.icon_step}>
-                      <use xlinkHref={`${svg}#icon-package`} />
-                    </svg>
-                  </div>
-                </div>
-                <h3 className={s.step_title}>Make your order</h3>
-                <p className={s.step_text}>
-                  add the product to the cart and fill in all the necessary
-                  fields to complete the order
-                </p>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className={s.step_wrapper}>
-              <div className={s.number_wrapper}>
-                <div className={s.number}>
-                  <p>4</p>
-                </div>
-              </div>
-              <div className={s.step}>
-                <div className={s.padding_wrapper}>
-                  <div className={s.icon_step_wrapper}>
-                    <svg className={s.icon_step}>
-                      <use xlinkHref={`${svg}#icon-phone-call`} />
-                    </svg>
-                  </div>
-                </div>
-                <h3 className={s.step_title}>The manager will contact you</h3>
-                <p className={s.step_text}>
-                  Our manager will contact you to clarify the order, then you
-                  will receive an invoice for payment. After payment, we'll send
-                  the drone to the address you specified on the same day.
-                </p>
-              </div>
-            </div>
-          </li>
-        </ul>
-      )}
+          ))}
+        </Swiper>
+      </div>
     </section>
   );
 };
