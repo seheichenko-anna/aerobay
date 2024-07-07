95516dd270f83d6efcc5c6f368dc2b98c43a086b
Rozygeldi94 <rozygeldi1994@gmail.com>
2024-07-07 22:28:35 +0300

    some files change

---
+++ src/components/AllProductsSection/AllProducts/index.tsx
@@ -0,0 +1,7 @@
+export const AllProducts = () => {
+  return (
+    <main>
+      <article></article>
+    </main>
+  );
+};
--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -1,5 +1,23 @@
+import c from './Catalog.module.scss';
+import { Breadcrumbs } from '../../components/Breadcrumbs';
+import { CategoryOfDrones } from '../../components/CategoryOfDrones';
+import { FilterProducts } from '../../components/AllProductsSection/Sidebar';
+import { AllProducts } from '../../components/AllProductsSection/AllProducts';
+
 const Catalog = () => {
-  return <div>Catalog</div>;
+  return (
+    <>
+      <Breadcrumbs />
+      <section>
+        <CategoryOfDrones />
+      </section>
+      <section className={c.all_products_wrap}>
+        <FilterProducts />
+        <AllProducts />
+      </section>
+      );
+    </>
+  );
 };
 
 export default Catalog;
