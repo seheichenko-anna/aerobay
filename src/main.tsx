8c51738a21f69a9e3941e17dcf81c5b3e78cedec
seheichenko-anna <seheichenko.anna@gmail.com>
2024-08-12 16:15:34 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter basename="aerobay">
+    <HashRouter>
       <Provider store={store}>
         <App />
         <ToastContainer autoClose={1500} />
