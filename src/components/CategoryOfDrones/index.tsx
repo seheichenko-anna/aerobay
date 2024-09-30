c909b1eb3e58954818bdc1c7b4ad4e4145ee51dd
Rozygeldi Halykov <rozygeldi1994@gmail.com>
2024-10-01 00:27:42 +0300

    Catalog page (#76)
    * tr
    
    * tr #2
    
    * tr #3
    
    * tr #4
    
    * fixed images path address
    
    * updated sidebar
    
    * test 02082024
    
    * added logic products & pagination & show-more btn
    
    * added logic products & pagination & show-more btn #3
    
    * Update package.json
    
    * Update Dropdown2.tsx
    
    * Update index.tsx
    
    * Update index.tsx
    
    * Update package.json
    
    * Update package-lock.json
    
    * Update AllProducts.module.scss
    
    * Update index.tsx
    
    * mobile version of catalog-page
    
    * Update index.tsx
    
    * Update Dropdown2.tsx
    
    * Update index.tsx
    
    * Update package-lock.json
    
    * mobile version of catalog-page #2
    
    * Update package-lock.json
    
    * Update index.tsx
    
    * Update index.tsx

--- src/components/CategoryOfDrones/index.tsx
+++ src/components/CategoryOfDrones/index.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import c from './CategoryOfDrones.module.scss';
 import {
   dronesImg,
@@ -56,46 +56,4 @@ export const CategoryOfDrones = () => {
     </section>
   );
 };
-=======
-import c from './CategoryOfDrones.module.scss';
-import dronesImg from '../../assets/catalog/category-of-drones/drone.png';
-import accessoriesImg from '../../assets/catalog/category-of-drones/accessories.png';
-import customizeDroneImg from '../../assets/catalog/category-of-drones/customize_drone.png';
 
-const categoryOfDrones = [
-  {
-    id: 1,
-    title: 'Drones',
-    href: '/',
-    imagePath: dronesImg,
-  },
-  {
-    id: 2,
-    title: 'Accessories',
-    href: '/',
-    imagePath: accessoriesImg,
-  },
-  {
-    id: 3,
-    title: 'Customize drone',
-    href: '/',
-    imagePath: customizeDroneImg,
-  },
-];
-
-export const CategoryOfDrones = () => {
-  return (
-    <>
-      <h1 className={c.main_title}>Catalog</h1>
-      <div className={c.category_of_drones}>
-        {categoryOfDrones?.map(category => (
-          <div key={category?.id} className={c.category_of_drones__category}>
-            <img src={category?.imagePath} alt={category?.title} />
-            <p>{category?.title}</p>
-          </div>
-        ))}
-      </div>
-    </>
-  );
-};
->>>>>>> 07e05e0aaf6f06be0e9e5653bc18e3c8df61f090
