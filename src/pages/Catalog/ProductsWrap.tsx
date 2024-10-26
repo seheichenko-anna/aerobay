86f876e7235cf33b834c22e3baaf06ec8de208c9
dromse <dromasko4@gmail.com>
2024-10-26 23:43:36 +0300

    move useSorted to file, fix subcategory type

--- src/pages/Catalog/ProductsWrap.tsx
+++ src/pages/Catalog/ProductsWrap.tsx
@@ -1,45 +1,13 @@
 import { useContext } from 'react';
 import { Products } from '../../components/Products';
-import { BaseProduct } from '../../redux/types';
 import styles from './Catalog.module.scss';
 import { CatalogContext } from './CatalogProvider';
-import { SortProvider, useSort } from './SortProvider';
+import { SortProvider } from './SortProvider';
 import AccessoriesFilters from './filters/AccessoriesFilters';
 import DronesFilters from './filters/DronesFilters';
-import { SortByItems } from './sortByItems';
 import useAccessories from './useAccessories';
 import useDrones from './useDrones';
-
-const useSorted = (products: BaseProduct[]) => {
-  const { currentSort } = useSort()!;
-  return products.slice().sort(sortItemsByPrice(currentSort));
-};
-
-const sortItemsByPrice =
-  (sortType: SortByItems) => (item1: BaseProduct, item2: BaseProduct) => {
-    if (sortType === 'Low To High') {
-      return item1.price - item2.price;
-    }
-
-    if (sortType === 'High To Low') {
-      return item2.price - item1.price;
-    }
-
-    if (sortType === 'New') {
-      return (
-        Number(new Date(item2.created_at)) - Number(new Date(item1.created_at))
-      );
-    }
-
-    if (sortType === 'Sale') {
-      return (
-        Number(item2.discount) -
-        Number(item1.discount)
-      );
-    }
-
-    return 0;
-  };
+import useSorted from './useSorted';
 
 const Drones = () => {
   const drones = useSorted(useDrones());
--- src/pages/Catalog/filters/DronesFilters.tsx
+++ src/pages/Catalog/filters/DronesFilters.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CheckboxGroup } from '../../../components/Products/Sidebar/CheckboxGroup';
 import { getCheckboxOptions } from '../../../components/Products/Sidebar/filterOptions/getCheckboxOptions';
 import {
@@ -7,6 +7,22 @@ import {
   engineTypeStringsWithBoolean,
   flightDistanceStringsWithBoolean,
 } from '../filterOptions';
+import { fetchSubcategoriesThunk } from '../../../redux/subcategories/subcategoriesOperations';
+import { selectSubcategories } from '../../../redux/subcategories/subcategoriesSlice';
+import { useAppDispatch } from '../../../redux/hooks/useAppDispatch';
+import { useAppSelector } from '../../../redux/hooks/useAppSelector';
+
+function useSubcategories() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchSubcategoriesThunk());
+  }, [dispatch]);
+
+  const subcategories = useAppSelector(selectSubcategories);
+
+  return subcategories;
+}
 
 const DronesFilters = () => {
   const engineTypeOptions = getCheckboxOptions(
@@ -25,12 +41,21 @@ const DronesFilters = () => {
     ...useState(flightDistanceStringsWithBoolean),
   );
 
+  // TODO: load engine type options
+  // from subcategories, load filters where we don't have category or if have drones category
+  const subcategories = useSubcategories();
+  console.log(
+    subcategories.filter(
+      sub => sub.category === null || sub.category.name === 'Drones',
+    ),
+  );
+
   return (
     <>
-      <CheckboxGroup title="Engine type" options={engineTypeOptions} />
-      <CheckboxGroup title="Battery life" options={batteryLifeOptions} />
-      <CheckboxGroup title="Battery" options={batteryOptions} />
-      <CheckboxGroup title="Flight distance" options={flightDistanceOptions} />
+      <CheckboxGroup title='Engine type' options={engineTypeOptions} />
+      <CheckboxGroup title='Battery life' options={batteryLifeOptions} />
+      <CheckboxGroup title='Battery' options={batteryOptions} />
+      <CheckboxGroup title='Flight distance' options={flightDistanceOptions} />
     </>
   );
 };
