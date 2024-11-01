ef4f0f22e3c974899c77f856b93c635d8a430ff8
dromse <dromasko4@gmail.com>
2024-11-01 21:58:00 +0200

    change eslint to find only ts,tsx files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,41 @@
-// @ts-check
-
-import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js';
+const __dirname = import.meta.dirname;
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      'no-warning-comments': [
+        'error',
+        {
+          terms: [
+            'todo',
+            'fix',
+            'bug',
+            'perf',
+            'warn',
+            'test',
+            'hack',
+            'note',
+          ],
+        },
+      ],
+      'no-console': 'error',
+    },
+  },
+
+  { ignores: ['dist/*', '*.cjs'] },
 );
