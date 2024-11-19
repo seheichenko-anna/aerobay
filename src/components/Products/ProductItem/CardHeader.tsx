34ba13dc8bbe0ab312f8302bf02bf2fafc613764
dromse <dromasko4@gmail.com>
2024-11-19 10:05:51 +0200

    add function to toggle item in compare state

--- src/components/Products/ProductItem/CardHeader.tsx
+++ src/components/Products/ProductItem/CardHeader.tsx
@@ -2,12 +2,36 @@ import { Tooltip } from 'react-tooltip';
 import c from '../Products.module.scss';
 
 import compareImg from '../../../assets/catalog/all-products/compare_arrow.svg';
+import {
+  addComparisonProduct,
+  deleteComparisonProduct,
+} from '../../../redux/comparisonProducts/comparisonProductsSlice';
+import { Drone } from '../../../redux/drones/dronesOperations';
+import { useAppDispatch } from '../../../redux/hooks/useAppDispatch';
+import { useAppSelector } from '../../../redux/hooks/useAppSelector';
 import { BaseProduct } from '../../../redux/types';
 
 const isNewItemByCreatedAt = (created_at: string) =>
   new Date(created_at) > new Date(2024, 9, 14);
 
 const ItemHeader = ({ product }: { product: BaseProduct }) => {
+  const dispatch = useAppDispatch();
+  const comparisonProducts = useAppSelector(
+    state => state.comparisonProducts.products,
+  );
+
+  const handleToggleCompare = () => {
+    const isProductInComparisonState = comparisonProducts.some(
+      comPro => comPro.id === product.id,
+    );
+
+    if (isProductInComparisonState) {
+      dispatch(deleteComparisonProduct(product as Drone));
+    }
+
+    dispatch(addComparisonProduct(product as Drone));
+  };
+
   return (
     <div className={c['product-card_img-section']}>
       <div className={c['product-card_top-section']}>
@@ -31,6 +55,7 @@ const ItemHeader = ({ product }: { product: BaseProduct }) => {
           className={c['product-card_compare']}
           data-tooltip-id='compare-tooltip'
           data-tooltip-content='Compare'
+          onClick={handleToggleCompare}
         >
           <img src={compareImg} alt='compare the product' />
         </div>
